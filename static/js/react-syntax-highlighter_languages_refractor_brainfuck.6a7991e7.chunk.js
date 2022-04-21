a271b4f251094cd371b244d7f9e789e1f46718e3
Falci Angelo <a.falci@live.it>
2022-04-21 17:09:38 +0200

    Updates

---
+++ static/js/react-syntax-highlighter_languages_refractor_brainfuck.6a7991e7.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_guide=self.webpackChunkreact_guide||[]).push([[5539],{2689:function(e){function a(e){e.languages.brainfuck={pointer:{pattern:/<|>/,alias:"keyword"},increment:{pattern:/\+/,alias:"inserted"},decrement:{pattern:/-/,alias:"deleted"},branching:{pattern:/\[|\]/,alias:"important"},operator:/[.,]/,comment:/\S+/}}e.exports=a,a.displayName="brainfuck",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_brainfuck.6a7991e7.chunk.js.map
---
+++ static/js/react-syntax-highlighter_languages_refractor_hsts.e0ce5298.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_guide=self.webpackChunkreact_guide||[]).push([[3140],{7761:function(e){function a(e){e.languages.hsts={directive:{pattern:/\b(?:max-age=|includeSubDomains|preload)/,alias:"keyword"},safe:{pattern:/\d{8,}/,alias:"selector"},unsafe:{pattern:/\d{1,7}/,alias:"function"}}}e.exports=a,a.displayName="hsts",a.aliases=[]}}]);
+//# sourceMappingURL=react-syntax-highlighter_languages_refractor_hsts.e0ce5298.chunk.js.map
