c8e072a3653f8bf1aa1db1b9e40e01992d7fd1a2
Falci Angelo <a.falci@live.it>
2022-05-14 14:58:45 +0200

    UseSyncExternalStore done

--- src/pages/hooks/useSyncExternalStore/demos/simpleCase.jsx
+++ src/pages/hooks/useSyncExternalStore/demos/simpleCase.jsx
@@ -3,8 +3,10 @@ function SimpleCase() {
     return (
         <React.Fragment>
             <h5>Caso Semplice</h5>
-
-            <iframe height="300" style={{ width: "100%" }} scrolling="no" title="useSyncExternalStore simple case" src="https://codepen.io/FAngelo94/embed/rNJMJPW?default-tab=js%2Cresult&editable=true" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+            <p>In questo esempio molto semplice il nostro store esterno è rappresentato dall'evento di "resize" associato alla window.</p>
+            <p>Come secondo parametro dello hook andiamo a specificare il valore che vogliamo leggere dallo "store" ogni volta che si aggiorna. In questo caso si tratta della larghezza della schermata.</p>
+            <p>Cambiando la larghezza del vostro schermo potrete vedere il valore aggiornarsi</p>
+            <iframe height="400" style={{ width: "100%" }} scrolling="no" title="useSyncExternalStore simple case" src="https://codepen.io/FAngelo94/embed/rNJMJPW?default-tab=js%2Cresult&editable=true" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
                 See the Pen <a href="https://codepen.io/FAngelo94/pen/rNJMJPW">
                     useSyncExternalStore simple case</a> by Angelo Falci (<a href="https://codepen.io/FAngelo94">@FAngelo94</a>)
                 on <a href="https://codepen.io">CodePen</a>.
--- src/pages/hooks/useSyncExternalStore/index.jsx
+++ src/pages/hooks/useSyncExternalStore/index.jsx
@@ -11,7 +11,14 @@ function UseSyncExternalStore() {
       title={"useSyncExternalStore"}
       theory={
         <React.Fragment>
-
+          <p><code>useSyncExternalStore</code> è un hook consigliato per la lettura e la sottoscrizione a sorgenti di dati di origine esterna in un modo compatibile con le funzionalità di rendering simultanee come <code>selective hydration</code> and <code>time slicing</code></p>
+          <p>Questo metodo restituisce il valore del negozio e accetta tre argomenti:</p>
+          <p><code>subscribe</code>: funzione per registrarsi ad una callback che viene chiamata ogni volta che cambia lo store esterno da cui leggiamo i dati.</p>
+          <p><code>getSnapshot</code>: funzione che restituisce il valore corrente dello store.</p>
+          <p><code>getServerSnapshot</code>: funzione che restituisce lo snapshot utilizzato durante il rendering del server.</p>
+          <CodeBlock
+            text={`const state = useSyncExternalStore(store.subscribe, store.getSnapshot);`}
+          />
         </React.Fragment>
       }
       demos={
@@ -21,7 +28,7 @@ function UseSyncExternalStore() {
       }
       errors={
         <React.Fragment>
-          
+
         </React.Fragment>
       }
     />
